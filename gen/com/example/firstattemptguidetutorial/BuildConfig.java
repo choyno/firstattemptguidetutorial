ef982b86fc627442818fec16b282c1028c1222b4
Ariel <ariel.lopezf@gmail.com>
2014-07-01 11:04:39 +0800

    first commit

---
+++ gen/com/example/firstattemptguidetutorial/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.firstattemptguidetutorial;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
